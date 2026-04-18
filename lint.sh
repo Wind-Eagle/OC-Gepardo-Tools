3c6ad067a6ad4878446f38d5bf0adc8a5bad57d0
Alexander Kernozhitsky <sh200105@mail.ru>
2026-04-18 18:49:05 +0200

    Fix linter

--- lint.sh
+++ lint.sh
@@ -5,5 +5,5 @@ shopt -s globstar
 exec luacheck g/**/*.lua \
   --globals checkArg \
   --no-self \
-  --ignore '411/err' '431/err' '212/.*' \
+  --ignore '411/err' '431/err' '411/ok' '431/ok' '212/.*' \
   --codes
